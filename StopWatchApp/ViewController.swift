de16c788e1ad599c3ad75ec67ab129f43e863991
prisonedinwardrobe <qladovka@yandex.ru>
2018-04-27 17:23:10 +0500

    timer, TableView
    1) + TableView
    2) timer doesn't stop when TableView is being scrolled
    3)Reset button additions

--- StopWatchApp/ViewController.swift
+++ StopWatchApp/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //lapsTableView logic
+    var dictionaryOfLaps: [String : Int] = [ : ]
+    
     //@IBOutlets
     @IBOutlet weak var startButtonView: RoundButton!
     @IBOutlet weak var resetButtonView: RoundButton!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var lapsTableView: UITableView!
     
     //timer logic
     var timer = Timer()
@@ -25,10 +29,17 @@ class ViewController: UIViewController {
         timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 68.0, weight: .thin)
         startButtonView.customColors = (UIColor.salmonDimmed, UIColor.salmonBright)
         resetButtonView.customColors = (UIColor.greyDimmed, UIColor.greyBright)
+        
+        startButtonView.backgroundColor = UIColor.salmonBright
+        resetButtonView.backgroundColor = UIColor.greyBright
+        
+        lapsTableView.delegate = self
+        lapsTableView.dataSource = self
     }
     
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: RunLoopMode.commonModes)
     }
     
     @objc func updateTimer() {
@@ -55,15 +66,33 @@ extension ViewController {
             runTimer()
             startButtonView.setTitle("Pause", for: .normal)
         }
-        view.setNeedsLayout()
     }
     
     @IBAction func releaseResetButton(_ sender: UIButton) {
+        startButtonView.setTitle("Start", for: .normal)
         timer.invalidate()
         currentTimeSeconds = 0
         
         timeLabel.text = "00 : 00 : 00"
     }
     
+}
+
+// MARK: - tableView
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dictionaryOfLaps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = lapsTableView.dequeueReusableCell(withIdentifier: "lapsTableViewCell")
+        
+        cell?.textLabel?.text = Array(dictionaryOfLaps.keys)[indexPath.row]
+        cell?.detailTextLabel?.text = "\(Array(dictionaryOfLaps.values)[indexPath.row])"
+        
+        return cell!
+    }
 }
 
